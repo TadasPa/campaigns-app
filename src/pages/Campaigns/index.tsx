02a533efaba9b58806cc96ee070bb66eb1527904
Tadas Padvilikis <tapa@devlabers.com>
2021-01-03 15:09:27 +0200

    styled components started to work

--- src/pages/Campaigns/index.tsx
+++ src/pages/Campaigns/index.tsx
@@ -1,15 +1,14 @@
-import React from "react";
-import { FC } from "react";
+import React, { FC } from "react";
 import styled from "styled-components";
 import { space, SpaceProps } from "styled-system";
 
 interface ICampaigns extends SpaceProps {}
 
-const Campaigns: FC<ICampaigns> = () => {
-  return <>campaigns page</>;
-};
+const Campaigns: FC<ICampaigns> = ({ children, ...rest }) => (
+  <span {...rest}>campaigns page{children}</span>
+);
 
-const CampaignsContainer: FC<ICampaigns> = styled(Campaigns)`
+export const CampaignsContainer: FC<ICampaigns> = styled(Campaigns)`
   padding: 10px;
 
   ${space}
--- src/theme/globalStyles.ts
+++ src/theme/globalStyles.ts
@@ -2,8 +2,7 @@ import { createGlobalStyle } from "styled-components";
 
 const GlobalStyle = createGlobalStyle`
 body {
-    margin: 0;
-    font-family: "Gordita", Helvetica Neue, Helvetica,Arial, sans-serif;
+    font-family: Gordita, Helvetica Neue, Helvetica,Arial, sans-serif;
     font-size: 16px;
   }  
 `;
